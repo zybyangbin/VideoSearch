8f64e30e31d96d1f95501ba1b544d436dfeddd63
Yangbin Zhou <zyb@umd.com>
2018-10-19 20:38:21 -0400

    first commit

---
+++ src/components/search_bar.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+  render () {
+    return <input />;
+  }
+  onInputChange() {
+    
+  }
+}
+// const SearchBar = () => {
+//   return <input />;
+// };
+
+
+
+export default SearchBar;
---
+++ src/index.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+
+const API_KEY = 'AIzaSyC0Wu2uRuQm716v9raq6eaCqEx4J5dWigU';
+
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({ videos });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoDetail video={this.state.videos[0]}/>
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
+}
+
+
+
+
+// const App = () => {
+//   return (
+//     <div>
+//       <SearchBar />
+//     </div>
+//   );
+// }
+
+ReactDOM.render(<App />, document.querySelector('.container'));
