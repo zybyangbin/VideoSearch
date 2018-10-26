f6dbf82ac8adf91799c73a8cab1e89d0105dc45d
Yangbin Zhou <zyb@umd.com>
2018-10-26 01:42:50 -0400

    video selection

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
-  render () {
-    return <input />;
+  constructor(props) {
+    super(props);
+    this.state = {term: ''}
   }
-  onInputChange() {
-    
+
+  render () {
+    return (
+      <div className="search-bar">
+      <input
+        value={this.state.term}
+        onChange={event => this.setState({term: event.target.value})}
+       />
+      </div>
+    )
+  }
+
+
+  onInputChange(term) {
+
+
   }
 }
 // const SearchBar = () => {
--- src/index.js
+++ src/index.js
@@ -12,10 +12,16 @@ const API_KEY = 'AIzaSyC0Wu2uRuQm716v9raq6eaCqEx4J5dWigU';
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { videos: [] };
+    this.state = {
+                  videos: [],
+                  selectedVideo: null
+                  };
 
     YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
-      this.setState({ videos });
+      this.setState({
+                      videos:videos,
+                      selectedVideo: videos[0]
+                     });
     });
   }
 
@@ -23,8 +29,10 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
-        <VideoDetail video={this.state.videos[0]}/>
-        <VideoList videos={this.state.videos} />
+        <VideoDetail video={this.state.selectedVideo}/>
+        <VideoList
+          onVideoSelect = {selectedVideo => this.setState({selectedVideo: selectedVideo})}
+          videos={this.state.videos} />
       </div>
     );
   }
